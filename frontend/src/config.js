5d9fd618c92067d3a9f3e48931da9ecddbfa9a54
Alphax16 <alphax7004@gmail.com>
2023-12-01 18:51:01 +0530

    Changed pickle's load to joblib's.

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -8,6 +8,9 @@
 // const API_BASE_URL = `${import.meta.env.VITE_BACKEND_URL}:${import.meta.env.VITE_BACKEND_PORT}` || "http://127.0.0.1:5000";
 const API_BASE_URL = "http://127.0.0.1:5000";
 // const API_BASE_URL = "https://crick-geek2.onrender.com";
+// const API_BASE_URL = "https://crick-geek.onrender.com";
+// const API_BASE_URL = "https://crick-geek-dplg.onrender.com";
+
 // console.log('API_BASE_URL:', API_BASE_URL);
 
 export const API_CONFIG = {
